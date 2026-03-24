101bf65e0c373b93e47a7caa2e786fbdf18502f6
amirsalla7 <amirsala2004sala@gmail.com>
2026-03-25 00:07:41 +0300

    update etl

--- mapping_engine.py
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit - ETL system"
-git branch -M main
-git remote add origin https://github.com/amirsalla7/Smart-Scheduler-ETL.git
-git push -u origin main
