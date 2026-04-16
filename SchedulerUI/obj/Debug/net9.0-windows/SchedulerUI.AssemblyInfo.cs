9e4918bf49ea8381f8f20ac47c682a51ea2c5574
amirsalla7 <amirsala2004sala@gmail.com>
2026-04-16 13:23:28 +0300

    update scheduler rules

--- SchedulerUI/obj/Debug/net9.0-windows/SchedulerUI.AssemblyInfo.cs
+++ SchedulerUI/obj/Debug/net9.0-windows/SchedulerUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchedulerUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dbb2362d8031a21809a9a29d5df41a55f52f070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd690b0ad6e2934ae7c97786823f5aa7722061b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchedulerUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchedulerUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
